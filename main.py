a3f7b2aa693401bb28ea7b15dd980cffb601da70
Ada Rapa <ada.rapa@interia.pl>
2020-04-22 03:02:13 +0200

    Change list'raport

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 
-masa_roquefort = 2
-cena_roquefort = 12.50
-roquefort = "Roquefort, %.2f kg, %.2f zł" % (masa_roquefort, cena_roquefort)
-
-masa_stilton = 1
-cena_stilton = 11.24
-stilton = "Stilton, %.2f kg, %.2f zł" % (masa_stilton, cena_stilton)
 
 masa_brie = 1
 cena_brie = 9.30
@@ -31,18 +24,12 @@ masa_hit = 0.22
 cena_hit = 122.32
 hit = "Hit, %.2f kg, %.2f zł" % (masa_hit, cena_hit)
 
-masa_listek_mietowy = 0.2
-cena_listek_mietowy = 20.00
-listek_mietowy = "Listek mietowy, %.2f kg, %.2f zł" % (masa_listek_mietowy, cena_listek_mietowy)
 
-masa_salami = 0.40
-cena_salami = 16.00
-salami = "Hit, %.2f kg, %.2f zł" % (masa_salami, cena_salami)
 
-suma = cena_roquefort + cena_stilton + cena_brie + cena_gouda + cena_edam + cena_parmezan + cena_mozzarella + cena_hit + cena_listek_mietowy + cena_salami
+suma = cena_brie + cena_gouda + cena_edam + cena_parmezan + cena_mozzarella + cena_hit
 suma_calkowita = "Suma zł: %.2f" % (suma)
 
-raport = "Raport z zakupów:\n %s \n %s \n %s \n %s \n %s \n %s \n %s \n %s \n %s \n %s \n %s" % (roquefort, stilton, brie, gouda, edam, parmezan, mozzarella, hit, listek_mietowy, salami, suma_calkowita )
+raport = "Raport z zakupów:\n %s \n %s \n %s \n %s \n %s \n %s \n %s " % (brie, gouda, edam, parmezan, mozzarella, hit, suma_calkowita )
 
 print(raport)
 
